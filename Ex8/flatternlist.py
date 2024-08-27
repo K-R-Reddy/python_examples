c3f8d4f5ebf2499e4281566fd1b1c71571998e6d
Karri Rajeswara Reddy <153280024+K-R-Reddy@users.noreply.github.com>
2024-08-27 22:05:39 +0530

    Update flatternlist.py
    First commit python

--- Ex8/flatternlist.py
+++ Ex8/flatternlist.py
@@ -1,2 +1,5 @@
-l=list(input("Enter any list : "))
-print(l)
+def flatten(lst):
+    return [item for sublist in lst for item in sublist]
+a=[[1,2,3,4],[9,7,5,3],[6,8],[10]]
+print("Actual List is",a)
+print("Flatten List is",flatten(a))
